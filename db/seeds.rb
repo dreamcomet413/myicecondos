3350f17431dfdbe44cbb1b20d2622921b46b13b1
Taras Zubyak <t.zubyak@gmail.com>
2015-11-11 18:40:01 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ m1 = MenuLocation.create(name: "Header", description: "Menu items appearing in t
 
 m1.menu_items.create(title: "About", url: "/about", sort_order: 1)
 m1.menu_items.create(title: "Buyers", url: "/listings", sort_order: 2)
-m1.menu_items.create(title: "Renters", url: "/listings", sort_order: 3)
+m1.menu_items.create(title: "Renters", url: "/listings?rent=1", sort_order: 3)
 m1.menu_items.create(title: "International", url: "/listings", sort_order: 4)
 m1.menu_items.create(title: "Location", url: "/listings", sort_order: 5)
 m1.menu_items.create(title: "Gallery", url: "/listings", sort_order: 6)
