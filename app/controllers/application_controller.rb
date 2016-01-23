2b52925db8449a35dbb9135608cad4435def41bf
Taras Zubyak <t.zubyak@gmail.com>
2016-01-22 19:08:15 -0500

    fix loging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if request.referer.include?("/listings")
+    if request.referer && request.referer.include?("/listings")
       request.referer.include?("?") ? "#{request.referer}&map=true" : "#{request.referer}?map=true"
     else
       root_path
